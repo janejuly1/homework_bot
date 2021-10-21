3a375b315d53307347aba3d16b88953aa231efe2
Janejuly1 <janejuly1@gmail.com>
2021-10-21 17:53:05 +0300

    check homework satuses bot

--- homework.py
+++ homework.py
@@ -1,14 +1,20 @@
 import logging
 
-...
+import requests
+import os
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-CHAT_ID = ...
+from dotenv import load_dotenv
+import time
+import telegram
 
-...
+load_dotenv()
 
-RETRY_TIME = 300
+
+PRACTICUM_TOKEN = os.getenv('TOKEN')
+TELEGRAM_TOKEN = os.getenv('TOKEN')
+CHAT_ID = os.getenv('ACCOUNT_SID')
+
+RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 
 HOMEWORK_STATUSES = {
@@ -17,41 +23,103 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена, в ней нашлись ошибки.'
 }
 
+HOMEWORKS = {}
+
 
 def send_message(bot, message):
-    ...
+    """Informs user about the status of homework."""
+    bot.send_message(
+        chat_id=CHAT_ID,
+        message=message,
+    )
 
 
 def get_api_answer(url, current_timestamp):
-    ...
+    """Get json data from API."""
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    payload = {'from_date': current_timestamp}
+
+    try:
+        response = requests.get(ENDPOINT, headers=headers, params=payload)
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к основному API: {error}')
+        raise error
+
+    if response.status_code != 200:
+        logging.error('Ошибка при запросе к основному API: status_code != 200')
+        raise RuntimeError(
+            'Ошибка при запросе к основному API: status_code != 200')
+
+    try:
+        response = response.json()
+    except Exception as error:
+        logging.error(f'Ошибка парсинга JSON: {error}')
+        raise error
+
+    return response
 
 
 def parse_status(homework):
-    verdict = ...
-    ...
+    """Get a status of homework from json."""
+    status = homework['status']
+    homework_name = homework['homework_name']
+
+    if homework_name in HOMEWORKS and HOMEWORKS[homework_name] == status:
+        return None
+
+    if homework_name not in HOMEWORKS or HOMEWORKS[homework_name] != status:
+        HOMEWORKS[homework_name] = status
+
+    verdict = HOMEWORK_STATUSES[status]
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_response(response):
-    homeworks = response.get('homeworks')
-    ...
+    """Check correctness of json."""
+    if 'homeworks' not in response:
+        raise RuntimeError('Отсутствует обязательное поле "homeworks"')
+
+    if 'current_date' not in response:
+        raise RuntimeError('Отсутствует обязательное поле "current_date"')
+
+    for homework in response['homeworks']:
+        if 'homework_name' not in homework:
+            raise RuntimeError('Отсутствует обязательное поле "homework_name"')
+
+        if 'status' not in homework:
+            raise RuntimeError('Отсутствует обязательное поле "status"')
+
+        if homework['status'] not in HOMEWORK_STATUSES.keys():
+            status = homework['status']
+            raise RuntimeError(f'Неизвестный статус "{status}"')
+
+    return True
 
 
 def main():
+    """Launches check homework statuses bot ."""
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    ...
+
     while True:
         try:
-            ...
+            response = get_api_answer(ENDPOINT, current_timestamp)
+            check_response(response)
+            for homework in response['homeworks']:
+                message = parse_status(homework)
+                if message is not None:
+                    send_message(bot, message)
+
+            current_timestamp = int(time.time())
             time.sleep(RETRY_TIME)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            logging.error(message)
             time.sleep(RETRY_TIME)
             continue
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     main()
